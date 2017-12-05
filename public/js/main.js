ddb854df257e392aed3120f5e253b811ad3e153c
Andrii Nyzhnyk <andrij1510@gmail.com>
2017-12-06 00:39:52 +0200

    rewrite code function

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,42 @@
-const socket = io();
-dateUser = {};
+window.onload = function () {
+    const socket = io();
+    let myMap;
 
-const myModule = {
-    init: function () {
-        navigator.geolocation.getCurrentPosition( (position) => {
-            dateUser.lat = position.coords.latitude;
-            dateUser.lng = position.coords.longitude;
+    function createMap(lat, lng) {
+        myMap = L.map('mapid').setView([lat, lng], 13);
+        L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(myMap);
+    }
 
-            dateUser.userName = prompt('Hello! What is your name ?');
+    function addMarker(lat,lng) {
+        const marker = L.marker([lat,lng]).addTo(myMap);
+    }
 
-            if(dateUser.userName && dateUser.lat && dateUser.lng) {
-                socket.emit('addUser', dateUser);
-                this.mymap = L.map('mapid').setView([dateUser.lat, dateUser.lng], 13);
-                L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.mymap);
-                this.addMarker(dateUser.lat, dateUser.lng);
-            }
-        });
-    },
-    addMarker: function(lat, lng) {
-        const marker = L.marker([lat,lng]).addTo(this.mymap);
-    },
-    removeMarker: function () {
+    function removeMarker() {
 
     }
+
+    function getPosition() {
+        return new Promise(resolve => {
+            navigator.geolocation.getCurrentPosition( (position) => {
+                const lat = position.coords.latitude;
+                const lng = position.coords.longitude;
+                resolve({lat,lng});
+            });
+
+        });
+    }
+
+    (async function init() {
+        const userName = prompt('Hello! What is your name ?');
+        let position = await getPosition();
+
+        if(userName && position.lat && position.lng) {
+            // socket.emit('addUser', dateUser);
+            createMap(position.lat, position.lng);
+            addMarker(position.lat, position.lng);
+        }
+    })();
+
 };
-window.onload = myModule.init();
 
 
